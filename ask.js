80d090133d1a3e3e55f88ea482cd74e1aa803f1a
borkystal <345549196@qq.com>
2022-06-24 13:49:13 +0800

    优化代码

--- ask.js
+++ ask.js
@@ -1,23 +1,27 @@
 module.exports = [
-    {
-        type: 'confirm',
-            name: 'private',
-            message: 'ths resgistery is private?',
-    },
-    {
-        type: 'input',
-        name: 'author',
-        message: 'author?',
-    }, 
-    {
-        type: 'input',
-        name: 'description',
-        message: 'description?',
-    }, 
-    {
-        type: 'input',
-        name: 'license',
-        message: 'license?',
-    }
-
+  {
+    type: "input",
+    name: "projectname",
+    message: "what is your projectname?",
+  },
+  {
+    type: "confirm",
+    name: "private",
+    message: "ths resgistery is private?",
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "author?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "description?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "license?",
+  },
 ];
